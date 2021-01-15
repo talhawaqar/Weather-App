3642b5b7d339344957d65c7522ac5eb2388d16b5
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2021-01-15 17:20:46 +0500

    Add Readme md

--- src/dom.js
+++ src/dom.js
@@ -1,19 +1,36 @@
 import { weatherInfo } from './api-requests';
 let widgetDiv = document.getElementById('weather-info');
  
-const displayWeather = async (city) => {
+const displayWeather = async (city, centigradeUnit) => {
   let weather = await weatherInfo(city);
-  console.log(weather);
+  let unit = '';
+  let temperature = 0;
+  let feelLike = 0;
+
   if (weather.message == "city not found" ) {
     widgetDiv.innerHTML = `<h5>City not Found</h5>`;
+    document.getElementById('toggleUnit').style.visibility = 'hidden';
     return;  
+  }
+  document.getElementById('toggleUnit').style.visibility = 'visible';
+  if (centigradeUnit) {
+    unit = 'C';
+    temperature = Math.round(weather.main.temp - 273);
+    feelLike = Math.round(weather.main.feels_like - 273);
+    console.log(weather.main.feels_like);
+  }
+  else 
+  {
+    unit = 'F';
+    temperature = Math.round((weather.main.temp - 273) * (9/5) + 32);
+    feelLike = Math.round((weather.main.feels_like - 273) * (9/5) + 32);
   }
   widgetDiv.innerHTML = `
     <div class="card" style="width: 18rem;">
       <img src="https://www.countryflags.io/${weather.sys.country}/flat/64.png" class="card-img-top" style="width:64px; display:inline-block; margin:auto" alt="">
       <div class="card-body">
-        <h5 class="card-title"> ${weather.name} ${weather.sys.country} ${Math.round(weather.main.temp - 273)}°C</h5>
-        <p> ${weather['weather']['0']['description']} || feels like ${Math.round(weather.main.feels_like - 273)}°C || Humidity: ${weather.main.humidity} || wind speed: ${weather.wind.speed}</p>
+        <h5 class="card-title"> ${weather.name} ${weather.sys.country} ${temperature}°${unit}</h5>
+        <p> ${weather['weather']['0']['description']} || feels like ${feelLike}°${unit} || Humidity: ${weather.main.humidity} || wind speed: ${weather.wind.speed}</p>
       </div>
     </div>`
   ;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import {displayWeather} from './dom'
 
 const submitButton = document.getElementById('btnSubmit');
 const cityTxt = document.getElementById('citytxt');
+const toggleUnit = document.getElementById('toggleUnit');
+let centigradeUnit = true;
 
 submitButton.addEventListener('click', ()=>{
-  displayWeather(cityTxt.value);
+  displayWeather(cityTxt.value, centigradeUnit);
+});
+
+toggleUnit.addEventListener('click', (e) =>{
+  if(e.target.innerHTML =='Ferhanite'){
+    centigradeUnit = false;
+    e.target.innerHTML ='Centigrade';
+    displayWeather(cityTxt.value, centigradeUnit);
+  }
+  else {
+    centigradeUnit = true;
+    e.target.innerHTML ='Ferhanite';
+    displayWeather(cityTxt.value, centigradeUnit);
+  }
 });
