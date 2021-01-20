f5674c0b8ec8a6b605ed9a68287054dc1b2097ee
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2021-01-20 16:14:45 +0500

    Changes done required by the tse

--- src/api-requests.js
+++ src/api-requests.js
@@ -1,6 +1,8 @@
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const key = '&APPID=915030e0750a47e3644dab2f5d014e52';
+const GIFYAPIKEY = 'QUCK5K1zxPSp4P1KYmfb26YUYgIUgamw';
+const gifyUrl = `https://api.giphy.com/v1/gifs/search?api_key=${GIFYAPIKEY}&limit=1&q=`;
 
 const weatherInfo = async (city) => {
   try {
@@ -12,4 +14,14 @@ const weatherInfo = async (city) => {
   }
 };
 
-export default weatherInfo;
+const getImage = async (search) => {
+  try {
+    const response = await fetch(`${gifyUrl}${search}`, { mode: 'cors' });
+    const images = await response.json();
+    return images;
+  } catch (err) {
+    return err;
+  }
+};
+
+export { weatherInfo, getImage };
--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,4 @@
-import weatherInfo from './api-requests';
+import { weatherInfo, getImage } from './api-requests';
 
 const widgetDiv = document.getElementById('weather-info');
 
@@ -7,10 +7,12 @@ const displayWeather = async (city, centigradeUnit) => {
   let unit = '';
   let temperature = 0;
   let feelLike = 0;
-
+  // console.log(weather.weather[0]);
   if (weather.message === 'city not found') {
     widgetDiv.innerHTML = '<h5>City not Found</h5>';
     document.getElementById('toggleUnit').style.visibility = 'hidden';
+    const container = document.getElementById('weather-widget');
+    container.style.cssText = 'background-color: gray !important';
     return;
   }
   document.getElementById('toggleUnit').style.visibility = 'visible';
@@ -24,13 +26,19 @@ const displayWeather = async (city, centigradeUnit) => {
     feelLike = Math.round((weather.main.feels_like - 273) * (9 / 5) + 32);
   }
   widgetDiv.innerHTML = `
-    <div class="card" style="width: 18rem;">
+    <div class="card" id="weather-card" style="width: 18rem;">
       <img src="https://www.countryflags.io/${weather.sys.country}/flat/64.png" class="card-img-top" style="width:64px; display:inline-block; margin:auto" alt="">
       <div class="card-body">
         <h5 class="card-title"> ${weather.name} ${weather.sys.country} ${temperature}°${unit}</h5>
         <p> ${weather.weather['0'].description} || feels like ${feelLike}°${unit} || Humidity: ${weather.main.humidity} || wind speed: ${weather.wind.speed}</p>
       </div>
     </div>`;
+  const weatherImage = await getImage(weather.weather[0].main.toLowerCase());
+
+  const container = document.getElementById('weather-widget');
+  const weathercard = document.getElementById('weather-card');
+  container.style.cssText = `background: url(${weatherImage.data['0'].images.original.url});, background-size: 100% 100%;`;
+  weathercard.style.cssText = 'background: transparent !important';
 };
 
 export default displayWeather;
